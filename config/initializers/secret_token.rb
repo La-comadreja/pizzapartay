ac3dfcbd05fc96f0a16b2b15a37486d32cedfbee
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-01-20 19:40:40 -0500

    Created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pizzapartay::Application.config.secret_token = '66d0478d26cc0a3df160c6dcad55759be10753eed7836dd60fa8ea43156b6e510a6a80221b5a6e132c50c01dfcb4c41547c5a22b114439a6dd35da4d76d7c3b8'
